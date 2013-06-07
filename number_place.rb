928b1c15d761049ec7cd8d4cd8ddd10b46b88b57
Kensuke Nagae <kyanny@gmail.com>
2013-06-07 23:47:28 +0900

    list_candidates

--- number_place.rb
+++ number_place.rb
@@ -14,23 +14,28 @@ class NumberPlace
     ALL_NUMBERS = Set.new(1..9)
 
     def list_candidates(x, y)
-      xs = [*1..9] - [x]
-      set = xs.reduce(nil){ |set, s|
-        if set.nil?
-          set = list_candidates_column_x(s)
-        else
-          set = set - list_candidates_column_x(s)
+      # ALL_NUMBERS - _list_candidates(x, 1..9) - _list_candidates(1..9, y)
+      ALL_NUMBERS - list_candidates_column(x) - list_candidates_row(y) - list_candidates_box(x, y)
+    end
+
+    def list_candidates_column(x)
+      Set.new((1..9).map{|i| @grid.cell(x, i).to_i}.reject{ |n| n.zero?})
+    end
+
+    def list_candidates_row(y)
+      Set.new((1..9).map{|i| @grid.cell(i, y).to_i}.reject{ |n| n.zero?})
+    end
+
+    def list_candidates_box(x, y)
+      xrange = [1..3, 4..6, 7..9].detect{ |range| range === x }
+      yrange = [1..3, 4..6, 7..9].detect{ |range| range === y }
+      candidates = []
+      yrange.each do |_y|
+        xrange.each do |_x|
+          candidates << @grid.cell(_x, _y).to_i
         end
-      }
-      # xs.map{ |n| list_candidates_column_x(n) }
-    end
-
-    def list_candidates_column_x(x)
-      ALL_NUMBERS - (1..9).map{|i| @grid.cell(x, i).to_i}
-    end
-
-    def list_candidates_column_y(y)
-      ALL_NUMBERS - (1..9).map{|i| @grid.cell(i, y).to_i}
+      end
+      Set.new(candidates.compact.reject{ |n| n.zero?})
     end
   end
 end
