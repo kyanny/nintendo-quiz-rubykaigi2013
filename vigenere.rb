6435d7c532d60b39a6048a570e59f2eb8c3e278f
Kensuke Nagae <kyanny@gmail.com>
2013-06-20 21:45:44 +0900

    encode_vigenere passed

--- vigenere.rb
+++ vigenere.rb
@@ -1,33 +1,22 @@
 require './utility.rb'
 
 def encode_vigenere(plaintext, key)
-  p '-'*80
   ciphertext = ''
-  lowers = Hash[('a'..'z').to_a.select{|char| char.match(/[a-z]/) }.map.with_index{ |char,index| [char, index] }]
-  uppers = Hash[('A'..'Z').to_a.select{|char| char.match(/[A-Z]/) }.map.with_index{ |char,index| [char, index] }]
-  enum = key.chars.cycle
+  houjin = Hash[('a'..'z').to_a.select{|char| char.match(/[a-z]/) }.map.with_index{ |char,index| [char, index] }]
+  enum = key.downcase.chars.cycle
 
   plaintext.each_char.with_index do |p, i|
     n = enum.next
-    houjin = if p.match(/[a-z]/)
-               lowers
+    upper = !!p.match(/[A-Z]/)
+    p = p.downcase
+    cipher = if houjin[p].nil? or houjin[n].nil?
+               p
              else
-               uppers
+               c = ((houjin[p] + houjin[n]) % houjin.length)
+               houjin.rassoc(c)[0]
              end
-    p([p, n, houjin[p], houjin[n]])
-    if houjin[p].nil? or houjin[n].nil?
-      ciphertext << p
-    else
-      c = ((houjin[p] + houjin[n]) % houjin.length)
-      p([p, n, houjin[p], houjin[n], houjin.length, c])
-      ciphertext << houjin.rassoc(c)[0]
-    end
-    # # p([p, n, c, chars.rassoc(c)])
-    # if chars.rassoc(c)
-    # else
-    #   ciphertext << p
-    # end
-    # # ciphertext << chars.rassoc(c)[0]
+    cipher = upper ? cipher.upcase : cipher
+    ciphertext << cipher
   end
   ciphertext
 end
